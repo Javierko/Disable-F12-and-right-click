8956462ff95f72bff2c8fa75fbd0ac53854776f0
Javierko <javierkoo21@gmail.com>
2020-07-02 18:49:58 +0200

    New syntax, typo

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).keydown(function(event) {
+	if(event.keyCode == 123) {
+		return false;
+	} else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+		return false;
+	}
+});
+
+document.onkeydown = function(e) {
+	if(e.keyCode === 123) {
+		return false;
+	}
+};
+
+const isNetScape = (navigator.appName == "Netscape") ? 1 : 0;
+
+if(isNetScape) {
+	document.captureEvents(Event.MOUSEDOWN || Event.MOUSEUP);
+}
+
+function mischandler() {
+	return false;
+}
+
+function mousehandler(e) {
+	let customEvent = (isNetScape) ? e : event;
+	let eventButton = (isNetScape) ? customEvent.which : customEvent.button;
+	
+	if(eventButton == 2 || eventButton == 3) {
+		return false;
+	}
+}
+
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
